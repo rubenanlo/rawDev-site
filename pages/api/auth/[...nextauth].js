fc15aa34a9cbaf573839ad5a2c6f55207db44e55
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-05-20 21:59:06 +0200

    Set up of nextauth with spotify as provider

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,73 @@
+import NextAuth from "next-auth";
+import SpotifyProvider from "next-auth/providers/spotify";
+import spotifyApi, { LOGIN_URL } from "library/spotify";
+
+const refreshAccessToken = async (token) => {
+  try {
+    // these methods come from spotify documentation?
+    // source: https://github.com/thelinmichael/spotify-web-api-node
+    spotifyApi.setAccessToken(token.accessToken);
+    spotifyApi.setRefresshToken(token.refreshToken);
+    const { body: refreshToken } = await spotifyApi.refreshAccessToken();
+    console.log(
+      "🚀 ~ file: [...nextauth].js:12 ~ refreshAccessToken ~ refreshToken:",
+      refreshToken
+    );
+    return {
+      ...token,
+      acessToken: refreshToken.access_token,
+      accessTokenExpires: Date.now + refreshToken.expires_in * 1000, // = 1 hour
+      refreshToken: refreshToken.refresh_token || token.refreshToken, // fall back to old refresh token
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+};
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    SpotifyProvider({
+      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+      authorization: LOGIN_URL,
+    }),
+    // ...add more providers here
+  ],
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    // Source: https://next-auth.js.org/v3/tutorials/refresh-token-rotation
+    async jwt({ token, account, user }) {
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000, //we are getting seconds, need milliseconds, therefore 1000
+        };
+      }
+      //   access token is still valid:
+      if (Date.now() < token.accessTokenExpires) {
+        console.log("token not expired");
+        return token;
+      }
+      //   access token expires:
+      return await refreshAccessToken(token);
+    },
+    async session({ session, token }) {
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.username = token.username;
+    },
+  },
+};
+
+export default NextAuth(authOptions);

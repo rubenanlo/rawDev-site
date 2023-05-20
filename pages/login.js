f535a6b76f2a31c41094942080919677e5624eae
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-05-20 21:59:26 +0200

    Add signIn ability with nextauth

---
+++ pages/login.js
@@ -0,0 +1,30 @@
+import { getProviders, signIn } from "next-auth/react";
+
+const login = ({ providers }) => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen w-full">
+      {/* I'm passing any providers with the code below, rather than using a custom button */}
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="border bg-slate-600 text-gray-100 rounded-md px-5 py-3"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+          >
+            Log In with {provider.name}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default login;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
+}

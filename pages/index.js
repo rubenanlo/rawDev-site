aea1158e3014f137b9563d4181c38ef8060986a1
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-05-20 21:41:04 +0200

    Delete files and content that we don't need

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,16 @@
-import AppLayout from "layouts/AppLayout";
+import Head from "next/head";
 
-const Home = () => (
-  <>
-    <AppLayout>
-      <main>
-        <div className="min-h-full">
-          <main>
-            <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8 ">
-              {/* Replace with your content */}
-              <div className="px-4 py-4 sm:px-0">
-                <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
-              </div>
-              {/* /End replace */}
-            </div>
-          </main>
-        </div>
-      </main>
-    </AppLayout>
-  </>
-);
+const Playlists = () => {
+  return (
+    <div className="border h-screen w-screen">
+      <Head>
+        <title></title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+      <h1 className="mx-auto">Playlists</h1>
+      <p>hello</p>
+    </div>
+  );
+};
 
-export default Home;
+export default Playlists;

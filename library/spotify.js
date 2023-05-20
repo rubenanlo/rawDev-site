b567fc0422612a25800c44c4ba55161daa0110a5
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-05-20 21:58:51 +0200

    Set up of the login url and the spotifyApi variable that comes with the scope for the queries needed to fetch specific data from spotify

---
+++ library/spotify.js
@@ -0,0 +1,35 @@
+import SpotifyWebApi from "spotify-web-api-node";
+
+const scopes = [
+  "user-read-private",
+  "user-read-email",
+  "user-read-playback-state",
+  "user-modify-playback-state",
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-top-read",
+  "streaming",
+  "user-read-playback-position",
+  "user-read-recently-played",
+  "user-library-read",
+  // "user-library-modify",
+  "user-follow-read",
+  // "user-follow-modify",
+  "playlist-read-private",
+  "playlist-read-collaborative",
+].join(",");
+
+const params = {
+  scope: scopes,
+};
+
+const queryParamString = new URLSearchParams(params);
+
+export const LOGIN_URL = `https://accounts.spotify.com/authorize?${queryParamString.toString()}`;
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+  clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+});
+
+export default spotifyApi;

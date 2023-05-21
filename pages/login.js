b5dd4bd6de185bb9f36fc188a2bffc5bc20198c3
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-05-21 20:40:17 +0200

    Fix typo for component name in login

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,6 @@
 import { getProviders, signIn } from "next-auth/react";
 
-const login = ({ providers }) => {
+const Login = ({ providers }) => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen w-full">
       {/* I'm passing any providers with the code below, rather than using a custom button */}
@@ -18,10 +18,11 @@ const login = ({ providers }) => {
   );
 };
 
-export default login;
+export default Login;
 
 export async function getServerSideProps() {
   const providers = await getProviders();
+
   return {
     props: {
       providers,

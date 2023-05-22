f3b8edeb898c11352a016cc555350ee9ebb563b9
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-05-22 22:54:02 +0200

    Deleted the sign out button from index to move it to playlists.

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,15 @@
-import { signOut } from "next-auth/react";
 import Head from "next/head";
 
-const Playlists = () => {
+const Homepage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Head>
         <title></title>
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-      <button
-        className="border bg-slate-600 text-gray-100 rounded-md px-5 py-3"
-        onClick={() => signOut()}
-      >
-        <p>Log out from Spotify</p>
-      </button>
+      <p>Homepage</p>
     </div>
   );
 };
 
-export default Playlists;
+export default Homepage;

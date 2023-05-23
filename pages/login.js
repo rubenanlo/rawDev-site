915d9f7d135f2fa6b8e2ddb0c234c254cf1670a6
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-05-23 21:33:23 +0200

    refactor code

--- pages/login.js
+++ pages/login.js
@@ -1,25 +1,23 @@
 import { getProviders, signIn } from "next-auth/react";
 import { getRedirectUrl } from "helpers/getRedirectUrl";
 
-const Login = ({ providers }) => {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen w-full">
-      {/* I'm passing any providers with the code below, rather than using a custom button */}
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button
-            className="border bg-slate-600 text-gray-100 rounded-md px-5 py-3"
-            onClick={() =>
-              signIn(provider.id, { callbackUrl: getRedirectUrl(provider.id) })
-            }
-          >
-            <p>Log In with {provider.name}</p>
-          </button>
-        </div>
-      ))}
-    </div>
-  );
-};
+const Login = ({ providers }) => (
+  <div className="flex flex-col items-center justify-center min-h-screen w-full">
+    {/* I'm passing any providers with the code below, rather than using a custom button */}
+    {Object.values(providers).map((provider) => (
+      <div key={provider.name}>
+        <button
+          className="border bg-slate-600 text-gray-100 rounded-md px-5 py-3"
+          onClick={() =>
+            signIn(provider.id, { callbackUrl: getRedirectUrl(provider.id) })
+          }
+        >
+          <p>Log In with {provider.name}</p>
+        </button>
+      </div>
+    ))}
+  </div>
+);
 
 export default Login;
 

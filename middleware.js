243a271a8a02d12af84e60d01c04a9f6aaa78e93
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-05-26 23:17:28 +0200

    updated routes in middleware

--- middleware.js
+++ middleware.js
@@ -16,16 +16,16 @@ export const middleware = async (req) => {
 
   // Redirect to login page if no token. Remember that
   // nextjs requires absolute urls
-  return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/login`);
+  return NextResponse.redirect(`${process.env.NEXTAUTH_URL}/`);
 };
 
 export const config = {
-  // This regex includes any path except /login. We also needed to exclude favicon.ico, api,
+  // This regex includes any path except /. We also needed to exclude favicon.ico, api,
   // _next/static and _next/image to allow the app to work properly. Otherwise,
   // the page never renders. Note that Nextjs, when rendering the app, it
   // renders the following pathnames by default: favicon.ico, _next/static,
   // _next/image and api. With the middleware above, and without excluding these
   // last pathnames, the middleware would redirect to /login, causing the app to
   // not work. Source: https://github.com/vercel/next.js/pull/42320/commits/a08905bae6a07f78832ec89c0fd75dc68ee919bf
-  matcher: "/((?!login|api|_next/static|_next/image|favicon.ico).*)",
+  matcher: "/((?!|api|_next/static|_next/image|favicon.ico).*)",
 };

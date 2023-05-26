ae2eaf5bf951a699775926351a07f2b6c2270841
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-05-26 23:04:32 +0200

    Refactor the code in the dashboard components

--- components/dashboard/Dashboard.js
+++ components/dashboard/Dashboard.js
@@ -1,22 +1,21 @@
 import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
 import { useRecoilState } from "recoil";
-import { playlistIdState } from "atoms/playlistAtom";
-import useSpotify from "helpers/useSpotify";
 import ProfileDropDown from "components/dashboard/sidebar/ProfileDropDown";
 import SearchBar from "components/dashboard/sidebar/SearchBar";
-import TopBarLayout from "layouts/dashboard/topbar/TopBarLayout";
 import ButtonOpenDrawer from "components/dashboard/sidebar/ButtonOpenDrawer";
 import SideBarMobile from "components/dashboard/sidebar/SideBarMobile";
-import SideBarMobileDrawer from "layouts/dashboard/sidebar/SideBarMobileDrawer";
 import SideBarDesktop from "components/dashboard/sidebar/SideBarDesktop";
-import { useSession } from "next-auth/react";
-import CenterBoardLayout from "layouts/dashboard/centerboard/CenterBoardLayout";
-import TopBarMobileLayout from "layouts/dashboard/topbar/TopBarMobileLayout";
 import MainContent from "components/dashboard/MainContent";
+import { playlistIdState } from "atoms/playlistAtom";
+import useSpotify from "helpers/useSpotify";
+import DashboardLayout from "layouts/DashboardLayout";
 
 const Dashboard = ({ providers }) => {
+  // console.log(providers);
   const { data: session } = useSession();
   const [playlists, setPlaylists] = useState([]);
+  console.log("🚀 ~ file: Dashboard.js:18 ~ Dashboard ~ playlists:", playlists);
   // const [playlistId, setPlaylistId] = useRecoilState(playlistIdState);
   const [searchedArtist, setSearchedArtist] = useRecoilState(playlistIdState);
   const spotifyApi = useSpotify();
@@ -36,27 +35,18 @@ const Dashboard = ({ providers }) => {
     }
   }, [session, spotifyApi, searchedArtist]);
 
-  console.log("🚀 ~ file: SideBar.js:25 ~ SideBar ~ playlists:", playlists);
+  // console.log("🚀 ~ file: SideBar.js:25 ~ SideBar ~ playlists:", playlists);
   // console.log(searchedArtist);
 
   return (
-    <>
-      <SideBarMobileDrawer>
-        <SideBarMobile />
-      </SideBarMobileDrawer>
+    <DashboardLayout>
+      <SideBarMobile />
       <SideBarDesktop />
-      <CenterBoardLayout>
-        <TopBarMobileLayout>
-          <ButtonOpenDrawer />
-          <TopBarLayout>
-            <SearchBar />
-            <ProfileDropDown providers={providers} />
-          </TopBarLayout>
-        </TopBarMobileLayout>
-
-        <MainContent />
-      </CenterBoardLayout>
-    </>
+      <ButtonOpenDrawer />
+      <SearchBar />
+      <ProfileDropDown providers={providers} />
+      <MainContent />
+    </DashboardLayout>
   );
 };
 
--- components/dashboard/MainContent.js
+++ components/dashboard/MainContent.js
@@ -1,6 +1,6 @@
 const MainContent = () => {
   return (
-    <main className="py-10">
+    <main className="py-10 h-screen overflow-y-scroll scrollbar-hide">
       <div className="px-4 sm:px-6 lg:px-8">hello</div>
     </main>
   );

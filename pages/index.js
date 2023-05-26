05b419f24d4627e415bf1ab4e12d5a0bb356d343
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-05-26 21:20:26 +0200

    Add getproviders in index site

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,30 @@
 import Head from "next/head";
+import { getProviders } from "next-auth/react";
+import Dashboard from "components/dashboard/Dashboard";
 
-const Homepage = () => {
+const Homepage = ({ providers }) => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <Head>
-        <title></title>
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-      </Head>
-      <p>Homepage</p>
-    </div>
+    <>
+      <Dashboard providers={providers} />
+      <div className="flex flex-col items-center justify-center h-screen">
+        <Head>
+          <title></title>
+          {/* <link rel="icon" href="/favicon.ico" /> */}
+        </Head>
+        <p>Homepage</p>
+      </div>
+    </>
   );
 };
 
 export default Homepage;
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}

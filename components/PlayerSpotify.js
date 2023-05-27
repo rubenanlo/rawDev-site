3a90584a03bcb4f2c00d637240b1017adb7447a7
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-05-27 23:07:30 +0200

    Set up spotify behavior in dashboard

---
+++ components/PlayerSpotify.js
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { ForwardIcon, PlayIcon } from "@heroicons/react/20/solid";
+import { BackwardIcon } from "@heroicons/react/24/solid";
+import { PauseIcon } from "@heroicons/react/24/outline";
+// import { currentSongIdState } from "atoms/songAtom";
+import useSpotify from "helpers/useSpotify";
+import { isPlayingState } from "atoms/songAtom";
+import { playlistIdState } from "atoms/playlistAtom";
+// import { pauseSong, playSong } from "../helpers/setPlayerSpotify";
+// import { startPlaying } from "../atoms/playlistAtom";
+
+const PlayerSpotify = () => {
+  // Get the current playlist ID from Recoil state
+  const playlistId = useRecoilValue(playlistIdState);
+  const [playlist, setPlaylist] = useState(playlistId);
+
+  // const isStartPlaying = useRecoilValue(startPlaying);
+
+  // Get the Spotify API instance
+  const spotifyApi = useSpotify();
+
+  // Fetch the playlist from Spotify API
+  const fetchPlaylist = async () => {
+    try {
+      const { body } = await spotifyApi.getPlaylist(playlistId);
+      setPlaylist(body);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPlaylist();
+  }, [playlistId, spotifyApi]);
+
+  // Get the current song playing state from Recoil state
+  // const [currentSongId, setCurrentSongId] = useRecoilState(currentSongIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+
+  // Function to play or pause the song
+  const playPauseSong = () => {
+    // setCurrentSongId(playlist?.tracks?.items?.[0].track.id);
+    setIsPlaying(!isPlaying);
+
+    // Pause the song if it is currently playing or
+    // Play the first song in the playlist if it is not playing
+    isPlaying
+      ? spotifyApi.pause()
+      : spotifyApi.play({
+          uris: [playlist?.tracks?.items?.[0].track?.uri],
+        });
+  };
+
+  return (
+    <div className="flex flex-col justify-center mb-10 w-52 mx-auto">
+      <Image
+        width={300}
+        height={300}
+        // to check if images exists below
+        src={playlist?.images?.[0].url}
+        alt="player"
+        className="group rounded-lg mb-10 shadow-md w-52 mx-auto"
+      />
+      <div className="flex justify-between px-2">
+        <button>
+          <BackwardIcon className="w-9 text-gray-900" />
+        </button>
+        <button onClick={playPauseSong}>
+          {isPlaying ? (
+            <PauseIcon className="w-9 text-gray-900" />
+          ) : (
+            <PlayIcon className="w-9 text-gray-900" />
+          )}
+        </button>
+        <button>
+          <ForwardIcon className="w-9 text-gray-900" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PlayerSpotify;

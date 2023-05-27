6a2a52db9f13f681f30535878007bc60fdf40870
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-05-27 23:10:28 +0200

    Create atom for currentsongid and isplayingstate

---
+++ atoms/songAtom.js
@@ -0,0 +1,10 @@
+import { atom } from "recoil";
+
+export const currentSongIdState = atom({
+  key: "currentSongIdState",
+  default: null,
+});
+export const isPlayingState = atom({
+  key: "isPlayingState",
+  default: false,
+});

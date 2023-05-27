b2ffe14396024c40f3fb96acf734e39274879b33
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-05-27 23:48:18 +0200

    change default playlist id

--- atoms/playlistAtom.js
+++ atoms/playlistAtom.js
@@ -2,7 +2,7 @@ import { atom } from "recoil";
 
 export const playlistIdState = atom({
   key: "playlistIdState",
-  default: "40PGoqMIeSkFgPaszx1ecA",
+  default: "37i9dQZF1DXbYM3nMM0oPk",
 });
 
 export const startPlaying = atom({

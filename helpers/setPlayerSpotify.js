966cd7d13b2703d4fd9d1b71bf7a31d76e48469a
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-05-29 22:40:11 +0200

    add currentplaying song function in setplayerspotify

--- helpers/setPlayerSpotify.js
+++ helpers/setPlayerSpotify.js
@@ -16,20 +16,34 @@ export const pauseSong = async (spotifyApi) => {
   }
 };
 
-export const skipSong = async (spotifyApi) => {
+export const getCurrentPlayingSong = async (spotifyApi) => {
   try {
-    await spotifyApi.skipToNext();
+    return await spotifyApi.getMyCurrentPlayingTrack();
   } catch (error) {
     console.log(error);
     throw error;
   }
 };
 
-export const previousSong = async (spotifyApi) => {
-  try {
-    await spotifyApi.skipToPrevious();
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
+// ********* //
+// The API does not work for skipping songs, so the following functions haven't
+// been used in the app. They are here for reference.
+
+// export const skipSong = async (spotifyApi, accessToken) => {
+//   try {
+//     console.log("yay");
+//     await spotifyApi.skipToNext(accessToken);
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// };
+
+// export const previousSong = async (spotifyApi) => {
+//   try {
+//     await spotifyApi.skipToPrevious();
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// };

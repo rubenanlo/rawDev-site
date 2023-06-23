5acf879265ed21f48488f08281b4d8d924605e5f
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-06-23 19:08:00 +0200

    Add navbar in app file

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,15 @@
+import { useEffect } from "react";
+import { Router } from "next/router";
 import Head from "next/head";
 import { TITLE, META_DESCRIPTION, META_IMAGE, URL } from "root/config";
 import { DefaultSeo } from "next-seo";
 import { SessionProvider } from "next-auth/react";
-import "styles/globals.css";
-import { useEffect } from "react";
-import { Router } from "next/router";
-import * as gtag from "helpers/gtag";
 import { RecoilRoot } from "recoil";
-import "tailwindcss/tailwind.css";
+import Navbar from "components/navbar/Navbar";
+import * as gtag from "helpers/gtag";
 import AppLayout from "layouts/AppLayout";
+import "tailwindcss/tailwind.css";
+import "styles/globals.css";
 
 const App = ({ Component, pageProps: { session, ...pageProps } }) => {
   // Track pages with google analytics
@@ -38,6 +39,7 @@ const App = ({ Component, pageProps: { session, ...pageProps } }) => {
       <SessionProvider session={session}>
         <RecoilRoot>
           <AppLayout>
+            <Navbar />
             <Component {...pageProps} />
           </AppLayout>
         </RecoilRoot>

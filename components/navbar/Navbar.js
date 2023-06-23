635bb7ba9e94329aabd4c476792d96a05c0cd17f
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-06-23 22:53:33 +0200

    Updated mobile version

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -1,32 +1,33 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Disclosure } from "@headlessui/react";
+import { Popover, Transition } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Logo from "components/Logo";
 import ContactMeMenu from "components/navbar/ContactMeMenu";
 import { mainNavigation, contactMe, clientPortal } from "static/navbar/NAVBAR";
 import { classNames } from "helpers/setClassNames";
+import { Fragment } from "react";
 
 const Navbar = () => {
   const { pathname } = useRouter();
   const fullNavigation = [...mainNavigation, clientPortal, ...contactMe];
 
   return (
-    <Disclosure as="nav">
+    <Popover as="nav">
       {({ open }) => (
         <div className="fixed bg-blue-primary z-10 pb-5">
           <div className="w-screen px-2 sm:px-6 lg:px-10">
             <div className="relative flex h-16 justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button */}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <Popover.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
                     <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                   )}
-                </Disclosure.Button>
+                </Popover.Button>
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
                 <div className="flex flex-shrink-0 items-center">
@@ -59,25 +60,34 @@ const Navbar = () => {
               </div>
             </div>
           </div>
-
-          <Disclosure.Panel className="sm:hidden bg-orange-quaternary w-3/4 rounded-r-xl transition ease-in-out duration-1000">
-            <div className="space-y-1 pb-4 pt-2">
-              {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
-              {fullNavigation.map(({ name, href }) => (
-                <Disclosure.Button
-                  as="a"
-                  key={name}
-                  href={href}
-                  className="block py-2 pl-3 pr-4 text-base font-medium text-gray-700"
-                >
-                  {name}
-                </Disclosure.Button>
-              ))}
-            </div>
-          </Disclosure.Panel>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-x-0"
+            enterTo="opacity-100 translate-x-1"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-x-0"
+            leaveTo="opacity-0 translate-x-1"
+          >
+            <Popover.Panel className="sm:hidden bg-orange-quaternary w-3/4 rounded-r-xl transition ease-in-out duration-1000">
+              <div className="space-y-1 pb-4 pt-2">
+                {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
+                {fullNavigation.map(({ name, href }) => (
+                  <Popover.Button
+                    as="a"
+                    key={name}
+                    href={href}
+                    className="block py-2 pl-3 pr-4 text-base font-medium text-orange-primary hover:bg-orange-tertiary"
+                  >
+                    {name}
+                  </Popover.Button>
+                ))}
+              </div>
+            </Popover.Panel>
+          </Transition>
         </div>
       )}
-    </Disclosure>
+    </Popover>
   );
 };
 

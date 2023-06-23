ed0e1845b572ea465e978bd946c76b6a45fd8806
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-06-23 11:46:38 +0200

    Updated different sizes in logo

--- components/Logo.js
+++ components/Logo.js
@@ -1,21 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
-import { isDarkMode } from "helpers/isDarkMode";
+import LOGO_LIGHT from "static/assets/logo-white.png";
 
-const Logo = () => {
-  const logo = isDarkMode()
-    ? "static/assets/logo-white.png"
-    : "static/assets/logo.png";
-  return (
+const Logo = ({ size }) => {
+  return size === "big" ? (
+    <Link href="/" className="flex h-16 shrink-0 items-center gap-x-2 ">
+      <Image className="h-10 w-auto" src={LOGO_LIGHT} alt="Your Company" />
+      <p className=" text-xl -mb-1 text-orange ">rawDev</p>
+    </Link>
+  ) : (
     <Link href="/" className="flex h-16 shrink-0 items-center gap-x-2 ">
       <Image
         width={400}
         height={400}
         className="h-6 w-auto"
-        src={logo}
+        src={LOGO_LIGHT}
         alt="rawDev"
       />
-      <p className="text-sm -mb-3 font-comforta">rawDev</p>
+      <p className="text-sm -mb-3 text-orange">rawDev</p>
     </Link>
   );
 };

bd176077851ad52d8e8b0838021a306db58c68f8
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-06-25 21:37:39 +0200

    Add animation to slide from top in flip card

--- components/FlipCard.js
+++ components/FlipCard.js
@@ -1,16 +1,30 @@
 import { classNames } from "helpers/setClassNames";
 import ImageSmall from "components/ImageSmall";
+import { useAnimate, useInView } from "framer-motion";
+import { useEffect } from "react";
 
 const FlipCard = ({ image }) => {
+  const [scope, animate] = useAnimate();
+  const isInview = useInView(scope);
+
+  useEffect(() => {
+    isInview &&
+      animate(
+        scope.current,
+        { y: [-100, 0], opacity: [0, 1] },
+        { duration: 2 }
+      );
+  }, [isInview, animate, scope]);
+
   return (
-    <div className="group [perspective:1000px] rounded-xl">
-      <div className="relative h-full flex flex-col rounded-xl [transform-style:preserve-3d] transition-all duration-500 group-hover:[transform:rotateY(180deg)]">
+    <div ref={scope} className="group [perspective:1000px] rounded-xl">
+      <div className="relative h-full flex flex-col rounded-xl [transform-style:preserve-3d] transition-all duration-500 group-hover:[transform:rotateY(180deg)] cursor-pointer">
         {/* front of the card */}
         <div className="bg-no-repeat bg-cover bg-center relative">
           <ImageSmall image={image} />
         </div>
         {/* back of the card */}
-        <div className="absolute flex flex-col justify-start pt-10 pl-2 rounded-xl h-full w-full bg-black [transform:rotateY(180deg)] [backface-visibility:hidden]">
+        <div className="absolute flex flex-col justify-start pt-10 pl-2 rounded-xl h-full w-full bg-black [transform:rotateY(180deg)] [backface-visibility:hidden] cursor-pointer">
           <h2
             className={classNames(
               image.headerPosition === "middle"

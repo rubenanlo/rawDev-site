ceef2646ee510b8a1ca3624b63794aaccae38227
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-06-27 15:01:11 +0200

    Add integrity section in homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Introduction from "components/homepage/Introduction";
 import Transparency from "components/homepage/Transparency";
+import Integrity from "components/homepage/Integrity";
+import Collaboration from "components/homepage/Collaboration";
 
 const Homepage = () => {
   return (
     <div className="h-screen">
       <Introduction />
+      <Integrity />
       <Transparency />
+      <Collaboration />
     </div>
   );
 };

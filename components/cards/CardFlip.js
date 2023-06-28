4446821d1394558e0b24af85edb09b517a9e4e70
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-06-28 10:31:33 +0200

    Delete link to homepage sections from intro

--- components/cards/CardFlip.js
+++ components/cards/CardFlip.js
@@ -3,7 +3,6 @@ import { useAnimate, useInView } from "framer-motion";
 import { motion } from "framer-motion";
 import CardImage from "components/cards/CardImage";
 import { classNames } from "helpers/setClassNames";
-import { Link as ScrollLink } from "react-scroll";
 
 const CardFlip = ({ image }) => {
   const [scope, animate] = useAnimate();
@@ -19,55 +18,53 @@ const CardFlip = ({ image }) => {
 
   return (
     <div ref={scope} className="group [perspective:1000px] rounded-xl">
-      <ScrollLink to={image.alt} smooth={true}>
-        <div className="relative h-full flex flex-col rounded-xl [transform-style:preserve-3d] transition-all duration-500 group-hover:[transform:rotateY(180deg)] cursor-pointer">
-          <motion.div
-            className="bg-no-repeat bg-cover bg-center relative"
-            // initial={{ rotateY: 0 }}
-            animate={
-              image.animation.type === "flip" && {
-                rotateY: [0, flipDirection],
-              }
+      <div className="relative h-full flex flex-col rounded-xl [transform-style:preserve-3d] transition-all duration-500 group-hover:[transform:rotateY(180deg)] cursor-pointer">
+        <motion.div
+          className="bg-no-repeat bg-cover bg-center relative"
+          // initial={{ rotateY: 0 }}
+          animate={
+            image.animation.type === "flip" && {
+              rotateY: [0, flipDirection],
             }
-            transition={{
-              duration: 0.5,
-              delay: image.animation.start || 2,
-              bounce: 0,
-            }}
-          >
-            {/* front of the card */}
-            <CardImage image={image} bounce="integrity" />
-            {/* back of the card */}
-          </motion.div>
-          <motion.div
-            className="absolute flex flex-col justify-start pt-10 pl-2 rounded-xl h-full w-full bg-black [transform:rotateY(180deg)] [backface-visibility:hidden] cursor-pointer"
-            animate={
-              image.animation.type === "flip" && {
-                rotateY: [0, flipDirection],
-              }
+          }
+          transition={{
+            duration: 0.5,
+            delay: image.animation.start || 2,
+            bounce: 0,
+          }}
+        >
+          {/* front of the card */}
+          <CardImage image={image} bounce="integrity" />
+          {/* back of the card */}
+        </motion.div>
+        <motion.div
+          className="absolute flex flex-col justify-start pt-10 pl-2 rounded-xl h-full w-full bg-black [transform:rotateY(180deg)] [backface-visibility:hidden] cursor-pointer"
+          animate={
+            image.animation.type === "flip" && {
+              rotateY: [0, flipDirection],
             }
-            transition={{
-              duration: 0.5,
-              delay: image.animation.start || 2,
-              bounce: 0,
-            }}
+          }
+          transition={{
+            duration: 0.5,
+            delay: image.animation.start || 2,
+            bounce: 0,
+          }}
+        >
+          <h2
+            className={classNames(
+              image.headerPosition === "middle"
+                ? "bottom-1/2"
+                : image.headerPosition === "bottom"
+                ? "bottom-10"
+                : "",
+              "absolute text-orange-tertiary text-xl"
+            )}
           >
-            <h2
-              className={classNames(
-                image.headerPosition === "middle"
-                  ? "bottom-1/2"
-                  : image.headerPosition === "bottom"
-                  ? "bottom-10"
-                  : "",
-                "absolute text-orange-tertiary text-xl"
-              )}
-            >
-              {image.alt}
-              <p></p>
-            </h2>
-          </motion.div>
-        </div>
-      </ScrollLink>
+            {image.alt}
+            <p></p>
+          </h2>
+        </motion.div>
+      </div>
     </div>
   );
 };

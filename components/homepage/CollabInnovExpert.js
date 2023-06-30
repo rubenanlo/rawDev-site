4841bd359472683fae9f1e03cf84163e3fd95bad
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-06-30 15:20:44 +0200

    Added animation for images

--- components/homepage/CollabInnovExpert.js
+++ components/homepage/CollabInnovExpert.js
@@ -1,4 +1,6 @@
+import { useRef } from "react";
 import Link from "next/link";
+import { useInView, motion } from "framer-motion";
 
 const sections = [
   {
@@ -34,6 +36,8 @@ const sections = [
 ];
 
 const CollabInnovExpert = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
   return (
     <div className="isolate overflow-hidden ">
       <div className="mx-auto max-w-7xl px-6 pb-96 pt-24 text-center sm:pt-32 lg:px-8">
@@ -43,20 +47,26 @@ const CollabInnovExpert = () => {
           </h2>
         </div>
         <div className="relative mt-6">
-          <p className="mt-6 text-lg leading-8 text-gray-400">
+          <p ref={ref} className="mt-6 text-lg leading-8 text-gray-400">
             We are dedicated to make sure your vision is represented in the best
             way possible.
           </p>
         </div>
       </div>
-      <div className="flow-root  pb-24 sm:pb-32">
+      <motion.div
+        animate={{
+          y: isInView && [20, 0],
+          transition: { duration: 1, delay: 0.5 },
+        }}
+        className="flow-root  pb-24 sm:pb-32"
+      >
         <div className="-mt-80">
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto grid max-w-2xl grid-cols-1 gap-8 lg:max-w-7xl lg:grid-cols-3">
               {sections.map((section) => (
                 <div
                   key={section.id}
-                  className="flex flex-col justify-between rounded-3xl bg-gray-100 p-8 shadow-xl ring-1 ring-gray-900/10 sm:p-10"
+                  className="flex flex-col justify-between rounded-3xl bg-gray-100 p-8  ring-1 ring-gray-900/10 sm:p-10"
                 >
                   <div>
                     <h3
@@ -74,13 +84,13 @@ const CollabInnovExpert = () => {
               <div className="flex flex-col items-start gap-x-8 gap-y-6 rounded-3xl p-8 ring-1 ring-gray-900/10 sm:gap-y-10 sm:p-10 lg:col-span-3 lg:flex-row lg:items-center">
                 <div className="lg:min-w-0 lg:flex-1 ">
                   <p className="mt-1 text-base leading-7 text-gray-400 text-center">
-                    If you want to learn more about{" "}
+                    Want to learn more about{" "}
                     <span className="text-orange-tertiary text-2xl">
                       rawDev
-                    </span>
-                    , you can check it out{" "}
+                    </span>{" "}
+                    ? click{" "}
                     <Link
-                      href="#"
+                      href="/about"
                       className="text-orange-tertiary underline underline-offset-4 text-2xl"
                     >
                       here
@@ -91,7 +101,7 @@ const CollabInnovExpert = () => {
             </div>
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };

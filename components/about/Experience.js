92aea3b526c2720eeae284f79fa0750f928345ef
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-07-08 23:44:08 +0200

    Updated the experience component

--- components/about/Experience.js
+++ components/about/Experience.js
@@ -1,13 +1,5 @@
-import Image from "next/image";
-import CSS from "static/assets/css3-alt.svg";
-import REACT from "static/assets/react.svg";
-import HTML from "static/assets/html5.svg";
-import JS from "static/assets/js.svg";
-import NEXT from "static/assets/next.svg";
-import NPM from "static/assets/npmjs.svg";
 import { forwardRef } from "react";
-
-const stack = [NEXT, REACT, JS, HTML, CSS, NPM];
+import ExperienceSnapshot from "components/about/experience/ExperienceSnapshot";
 
 const timeline = [
   {
@@ -73,71 +65,7 @@ const Experience = forwardRef((props, ref) => {
   return (
     <div id="experience" className="">
       <main className="isolate">
-        {/* Stats */}
-        <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8 lg:pb-40">
-          <div className="mx-auto max-w-3xl lg:mx-0">
-            <h2 className="text-3xl font-bold tracking-tight text-orange-quaternary sm:text-4xl">
-              A snapshot of my professional journey
-            </h2>
-            <p className="mt-6 text-base leading-7 text-gray-400">
-              Coming from an economics background, I understand that it may not
-              be the most attention-grabbing profile at first. However, as an
-              economist, I seized the opportunity to expand my role and automate
-              processes for my team, ensuring more time for critical thinking
-              and less on repetitive tasks.
-            </p>
-            <p ref={ref} className="mt-6 text-base leading-7 text-gray-400">
-              I&apos;m a self-taught developer who values learning from
-              colleagues and user feedback. Transitioning from lengthy code to a
-              modular, component-oriented approach has improved code quality and
-              facilitated better peer reviews.
-            </p>
-          </div>
-          <div className="mx-auto mt-16 flex max-w-2xl flex-col gap-8 lg:mx-0 lg:mt-20 lg:max-w-none lg:flex-row lg:items-end">
-            <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-50 p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start">
-              <p className="flex-none text-3xl font-bold tracking-tight text-blue-primary">
-                10+ years
-              </p>
-              <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
-                <p className="text-lg font-semibold tracking-tight text-blue-primary">
-                  of experience in business consulting and economics
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-900 p-8 sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-sm lg:flex-auto lg:flex-col lg:items-start lg:gap-y-28">
-              <p className="flex-none text-3xl font-bold tracking-tight text-white">
-                1+ years
-              </p>
-              <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
-                <p className="text-lg font-semibold tracking-tight text-white">
-                  as a web developer
-                </p>
-                <p className="mt-2 text-base leading-7 text-gray-400">
-                  Working as a web developer for more than a year now, but I
-                  have more than 10 years of experience building data
-                  visualizations based on excel following DRY and component
-                  oriented approach.
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gradient-to-r from-orange-primary to-orange-secondary p-8 sm:w-11/12 sm:max-w-xl sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-none lg:flex-auto lg:flex-col lg:items-start lg:gap-y-28">
-              <p className="flex-none text-3xl font-bold tracking-tight text-white">
-                My stack
-              </p>
-              <div className="sm:shrink lg:w-full lg:flex-none sm:grid sm:grid-cols-2 sm:items-center sm:justify-center sm:gap-x-5">
-                <p className="text-lg text-center sm:text-left font-semibold tracking-tight text-white">
-                  TechStack
-                </p>
-                <div className="mt-2 text-base leading-7 text-indigo-200 grid grid-cols-3 items-center">
-                  {stack.map((icon) => (
-                    <Image key={icon} src={icon} alt="stack" className="w-7" />
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
+        <ExperienceSnapshot ref={ref} />
         {/* Timeline section */}
         <div className="mx-auto mt-8 max-w-7xl px-6 lg:px-8">
           <div className="mx-auto grid max-w-2xl grid-cols-1 gap-8 overflow-hidden lg:mx-0 lg:max-w-none lg:grid-cols-4">

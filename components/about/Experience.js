2e71bb140ac26b63316b886e4c4a6a63bbcc6840
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-07-08 20:00:02 +0200

    INitial setup for experience section in about

--- components/about/Experience.js
+++ components/about/Experience.js
@@ -1,76 +1,245 @@
-import { Disclosure } from "@headlessui/react";
-import { MinusSmallIcon, PlusSmallIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import CSS from "static/assets/css3-alt.svg";
+import REACT from "static/assets/react.svg";
+import HTML from "static/assets/html5.svg";
+import JS from "static/assets/js.svg";
+import NEXT from "static/assets/next.svg";
+import NPM from "static/assets/npmjs.svg";
 import { forwardRef } from "react";
 
-const faqs = [
+const stack = [NEXT, REACT, JS, HTML, CSS, NPM];
+
+const timeline = [
   {
-    topic: "My skills",
-    answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+    name: "Founded company",
+    description:
+      "Nihil aut nam. Dignissimos a pariatur et quos omnis. Aspernatur asperiores et dolorem dolorem optio voluptate repudiandae.",
+    date: "Aug 2021",
+    dateTime: "2021-08",
   },
   {
-    topic: "Education",
-    answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+    name: "Secured $65m in funding",
+    description:
+      "Provident quia ut esse. Vero vel eos repudiandae aspernatur. Cumque minima impedit sapiente a architecto nihil.",
+    date: "Dec 2021",
+    dateTime: "2021-12",
   },
   {
-    topic: "Experience",
-    answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+    name: "Released beta",
+    description:
+      "Sunt perspiciatis incidunt. Non necessitatibus aliquid. Consequatur ut officiis earum eum quia facilis. Hic deleniti dolorem quia et.",
+    date: "Feb 2022",
+    dateTime: "2022-02",
   },
   {
-    topic: "Languages",
-    answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+    name: "Global launch of product",
+    description:
+      "Ut ipsa sint distinctio quod itaque nam qui. Possimus aut unde id architecto voluptatem hic aut pariatur velit.",
+    date: "Dec 2022",
+    dateTime: "2022-12",
   },
-  // More questions...
+];
+const roles = [
+  {
+    id: 1,
+    role: "Full-time designer",
+    href: "#",
+    description:
+      "Quos sunt ad dolore ullam qui. Enim et quisquam dicta molestias. Corrupti quo voluptatum eligendi autem labore.",
+    salary: "$75,000 USD",
+    location: "San Francisco, CA",
+  },
+  {
+    id: 2,
+    role: "Laravel developer",
+    href: "#",
+    description:
+      "Et veniam et officia dolorum rerum. Et voluptas consequatur magni sapiente amet voluptates dolorum. Ut porro aut eveniet.",
+    salary: "$125,000 USD",
+    location: "San Francisco, CA",
+  },
+  {
+    id: 3,
+    role: "React Native developer",
+    href: "#",
+    description:
+      "Veniam ipsam nisi quas architecto eos non voluptatem in nemo. Est occaecati nihil omnis delectus illum est.",
+    salary: "$105,000 USD",
+    location: "San Francisco, CA",
+  },
 ];
 
 const Experience = forwardRef((props, ref) => {
   return (
-    <div id="experience">
-      <div className="mx-auto sm:mr-0 sm:ml-auto max-w-7xl sm:w-4/5 px-6 lg:px-12 lg:py-40">
-        <div ref={ref} className="mx-auto max-w-4xl divide-y divide-gray-50">
-          <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-100">
-            My experience
-          </h2>
-          <dl className="mt-10 space-y-6 divide-y divide-gray-50/40">
-            {faqs.map((faq) => (
-              <Disclosure as="div" key={faq.question} className="pt-6">
-                {({ open }) => (
-                  <>
-                    <dt>
-                      <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-100">
-                        <span className="text-base font-semibold leading-7">
-                          {faq.topic}
-                        </span>
-                        <span className="ml-6 flex h-7 items-center">
-                          {open ? (
-                            <MinusSmallIcon
-                              className="h-6 w-6"
-                              aria-hidden="true"
-                            />
-                          ) : (
-                            <PlusSmallIcon
-                              className="h-6 w-6"
-                              aria-hidden="true"
-                            />
-                          )}
-                        </span>
-                      </Disclosure.Button>
-                    </dt>
-                    <Disclosure.Panel as="dd" className="mt-2 pr-12">
-                      <p className="text-base leading-7 text-gray-400">
-                        {faq.answer}
-                      </p>
-                    </Disclosure.Panel>
-                  </>
-                )}
-              </Disclosure>
-            ))}
-          </dl>
+    <div id="experience" className="">
+      <main className="isolate">
+        {/* Stats */}
+        <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8 lg:pb-40">
+          <div className="mx-auto max-w-3xl lg:mx-0">
+            <h2 className="text-3xl font-bold tracking-tight text-orange-quaternary sm:text-4xl">
+              A snapshot of my professional journey
+            </h2>
+            <p className="mt-6 text-base leading-7 text-gray-400">
+              Coming from an economics background, I understand that it may not
+              be the most attention-grabbing profile at first. However, as an
+              economist, I seized the opportunity to expand my role and automate
+              processes for my team, ensuring more time for critical thinking
+              and less on repetitive tasks.
+            </p>
+            <p ref={ref} className="mt-6 text-base leading-7 text-gray-400">
+              I&apos;m a self-taught developer who values learning from
+              colleagues and user feedback. Transitioning from lengthy code to a
+              modular, component-oriented approach has improved code quality and
+              facilitated better peer reviews.
+            </p>
+          </div>
+          <div className="mx-auto mt-16 flex max-w-2xl flex-col gap-8 lg:mx-0 lg:mt-20 lg:max-w-none lg:flex-row lg:items-end">
+            <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-50 p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start">
+              <p className="flex-none text-3xl font-bold tracking-tight text-blue-primary">
+                10+ years
+              </p>
+              <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+                <p className="text-lg font-semibold tracking-tight text-blue-primary">
+                  of experience in business consulting and economics
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-900 p-8 sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-sm lg:flex-auto lg:flex-col lg:items-start lg:gap-y-28">
+              <p className="flex-none text-3xl font-bold tracking-tight text-white">
+                1+ years
+              </p>
+              <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+                <p className="text-lg font-semibold tracking-tight text-white">
+                  as a web developer
+                </p>
+                <p className="mt-2 text-base leading-7 text-gray-400">
+                  Working as a web developer for more than a year now, but I
+                  have more than 10 years of experience building data
+                  visualizations based on excel following DRY and component
+                  oriented approach.
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gradient-to-r from-orange-primary to-orange-secondary p-8 sm:w-11/12 sm:max-w-xl sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-none lg:flex-auto lg:flex-col lg:items-start lg:gap-y-28">
+              <p className="flex-none text-3xl font-bold tracking-tight text-white">
+                My stack
+              </p>
+              <div className="sm:shrink lg:w-full lg:flex-none sm:grid sm:grid-cols-2 sm:items-center sm:justify-center sm:gap-x-5">
+                <p className="text-lg text-center sm:text-left font-semibold tracking-tight text-white">
+                  TechStack
+                </p>
+                <div className="mt-2 text-base leading-7 text-indigo-200 grid grid-cols-3 items-center">
+                  {stack.map((icon) => (
+                    <Image key={icon} src={icon} alt="stack" className="w-7" />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+
+        {/* Timeline section */}
+        <div className="mx-auto mt-8 max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto grid max-w-2xl grid-cols-1 gap-8 overflow-hidden lg:mx-0 lg:max-w-none lg:grid-cols-4">
+            {timeline.map((item) => (
+              <div key={item.name}>
+                <time
+                  dateTime={item.dateTime}
+                  className="flex items-center text-sm font-semibold leading-6 text-indigo-600"
+                >
+                  <svg
+                    viewBox="0 0 4 4"
+                    className="mr-4 h-1 w-1 flex-none"
+                    aria-hidden="true"
+                  >
+                    <circle cx={2} cy={2} r={2} fill="currentColor" />
+                  </svg>
+                  {item.date}
+                  <div
+                    className="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0"
+                    aria-hidden="true"
+                  />
+                </time>
+                <p className="mt-6 text-lg font-semibold leading-8 tracking-tight text-orange-quaternary">
+                  {item.name}
+                </p>
+                <p className="mt-1 text-base leading-7 text-gray-400">
+                  {item.description}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Experience */}
+        <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8">
+          <div className="mx-auto flex max-w-2xl flex-col items-end justify-between gap-16 lg:mx-0 lg:max-w-none lg:flex-row">
+            <div className="w-full lg:max-w-lg lg:flex-auto">
+              <h2 className="text-3xl font-bold tracking-tight text-orange-quaternary sm:text-4xl">
+                We’re always looking for awesome people to join us
+              </h2>
+              <p className="mt-6 text-xl leading-8 text-gray-400">
+                Diam nunc lacus lacus aliquam turpis enim. Eget hac velit est
+                euismod lacus. Est non placerat nam arcu. Cras purus nibh cursus
+                sit eu in id.
+              </p>
+              <img
+                src="https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1344&h=1104&q=80"
+                alt=""
+                className="mt-16 aspect-[6/5] w-full rounded-2xl bg-gray-50 object-cover lg:aspect-auto lg:h-[34.5rem]"
+              />
+            </div>
+            <div className="w-full lg:max-w-xl lg:flex-auto">
+              <h3 className="sr-only">Job openings</h3>
+              <ul className="-my-8 divide-y divide-gray-100">
+                {roles.map((role) => (
+                  <li key={role.id} className="py-8">
+                    <dl className="relative flex flex-wrap gap-x-3">
+                      <dt className="sr-only">Role</dt>
+                      <dd className="w-full flex-none text-lg font-semibold tracking-tight text-orange-quaternary">
+                        <a href={role.href}>
+                          {role.role}
+                          <span
+                            className="absolute inset-0"
+                            aria-hidden="true"
+                          />
+                        </a>
+                      </dd>
+                      <dt className="sr-only">Description</dt>
+                      <dd className="mt-2 w-full flex-none text-base leading-7 text-gray-400">
+                        {role.description}
+                      </dd>
+                      <dt className="sr-only">Salary</dt>
+                      <dd className="mt-4 text-base font-semibold leading-7 text-orange-quaternary">
+                        {role.salary}
+                      </dd>
+                      <dt className="sr-only">Location</dt>
+                      <dd className="mt-4 flex items-center gap-x-3 text-base leading-7 text-gray-500">
+                        <svg
+                          viewBox="0 0 2 2"
+                          className="h-0.5 w-0.5 flex-none fill-gray-300"
+                          aria-hidden="true"
+                        >
+                          <circle cx={1} cy={1} r={1} />
+                        </svg>
+                        {role.location}
+                      </dd>
+                    </dl>
+                  </li>
+                ))}
+              </ul>
+              <div className="mt-8 flex border-t border-gray-100 pt-8">
+                <a
+                  href="#"
+                  className="text-sm font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                >
+                  View all openings <span aria-hidden="true">&rarr;</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 });

ba9803931bf08657a9b764c360fbfad1303d6b50
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-07-19 15:02:56 +0200

    Added logic to include imported images and incos for tech in each project

---
+++ helpers/getProjects.js
@@ -0,0 +1,81 @@
+import fs from "fs";
+import path from "path";
+import yaml from "js-yaml";
+import { images } from "helpers/exportImages";
+
+// Get the distinct image keys without size suffixes
+const imagesArr = [
+  ...new Set(Object.keys(images).map((key) => key.replace(/_SM|_LG|_MD/g, ""))),
+];
+
+// Define the directory path
+const directory = path.join(
+  process.cwd(),
+  "public",
+  "static",
+  "about",
+  "projects"
+);
+
+// Function to get the file names from the directory
+const getFileNames = () =>
+  fs
+    .readdirSync(directory)
+    .filter((file) => file.endsWith(".yaml"))
+    .map((file) => file.replace(".yaml", ""));
+
+// Retrieve projects
+export const getProjects = () =>
+  getFileNames().map((fileName) => {
+    // Load the YAML file for the project
+    const project = yaml.load(
+      fs.readFileSync(`${directory}/${fileName}.yaml`, "utf8")
+    );
+    let updatedTechStack;
+    let imageProperties;
+    // Replace techStack icons with image objects
+    try {
+      updatedTechStack = project.techStack.map(({ icon, alt }) => {
+        const image = images[icon];
+        if (image) {
+          return { icon: image, alt };
+        }
+      });
+    } catch (error) {
+      console.log("You gorgot to add the techStack for this project");
+    }
+
+    // Merge the updated techStack with the project object
+    const updatedProject = {
+      ...project,
+      techStack: updatedTechStack,
+    };
+
+    // Find the corresponding image key for the current project
+    const coverImage = imagesArr.find((img) => fileName.toUpperCase() === img);
+
+    // Generate image properties based on available sizes
+    try {
+      imageProperties = ["_LG", "_MD", "_SM"].reduce((acc, size) => {
+        const imageUrl = images[`${coverImage}${size}`];
+        if (imageUrl) {
+          // Generate the property key dynamically based on size
+          const propertyKey = `imageUrl${size.replace(/_(.)/g, (_, char) =>
+            char.toUpperCase()
+          )}`;
+          acc[propertyKey] = imageUrl;
+        }
+        return acc;
+      }, {});
+    } catch (error) {
+      console.log(
+        "You forgot to add the image for this project, or you forgot to use the right naming convention for the image, please see the README file for more info"
+      );
+    }
+
+    // Merge the updatedProject object with the image properties
+    return {
+      ...updatedProject,
+      ...imageProperties,
+    };
+  });

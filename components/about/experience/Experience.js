2fe5df600563c0c73d4e12c4a6247d527b0dff09
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-07-19 17:13:19 +0200

    Fix some typos

--- components/about/experience/Experience.js
+++ components/about/experience/Experience.js
@@ -2,17 +2,14 @@ import { forwardRef } from "react";
 import ExperienceSnapshot from "components/about/experience/ExperienceSnapshot";
 import ExperienceTimeline from "components/about/experience/ExperienceTimeline";
 
-const Experience = forwardRef(({ experience: { timeline } }, ref) => {
-  console.log("🚀 ~ file: Experience.js:6 ~ Experience ~ timeline:", timeline);
-  return (
-    <div id="experience" ref={ref}>
-      <main className="isolate">
-        <ExperienceSnapshot />
-        <ExperienceTimeline timeline={timeline} />
-      </main>
-    </div>
-  );
-});
+const Experience = forwardRef(({ experience: { timeline } }, ref) => (
+  <div id="experience" ref={ref}>
+    <main className="isolate">
+      <ExperienceSnapshot />
+      <ExperienceTimeline timeline={timeline} />
+    </main>
+  </div>
+));
 
 Experience.displayName = "Experience";
 

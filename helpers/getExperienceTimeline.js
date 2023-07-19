126e4eaeaeac1df0635b7b0819110efc53eb503c
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-07-19 17:25:47 +0200

    refactor getexperiencetimeline code

--- helpers/getExperienceTimeline.js
+++ helpers/getExperienceTimeline.js
@@ -17,28 +17,15 @@ const getFileNames = () =>
     .filter((file) => file.endsWith(".yaml"))
     .map((file) => file.replace(".yaml", ""));
 
-export const getExperienceTimeline = () => {
-  const fileNames = getFileNames();
-  const timeline = [];
-
-  fileNames.forEach((fileName) => {
-    const data = yaml.load(
-      fs.readFileSync(`${directory}/${fileName}.yaml`, "utf8")
-    );
-
-    if (fileName === "education") {
-      data.forEach((study) => {
-        timeline.push({ ...study, tag: "education" });
-      });
-    } else if (fileName === "work") {
-      data.forEach((role) => {
-        timeline.push({ ...role, tag: "work" });
-      });
-    }
-  });
-
-  const experienceTimeline = timeline.sort(
-    (a, b) => dayjs(b.dateTime) - dayjs(a.dateTime)
-  );
-  return experienceTimeline;
-};
+export const getExperienceTimeline = () =>
+  getFileNames()
+    .reduce(
+      (acc, fileName) => [
+        ...acc,
+        ...yaml
+          .load(fs.readFileSync(`${directory}/${fileName}.yaml`, "utf8"))
+          .map((item) => ({ ...item, tag: fileName })),
+      ],
+      []
+    )
+    .sort((a, b) => dayjs(b.dateTime) - dayjs(a.dateTime));

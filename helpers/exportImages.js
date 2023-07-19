1d42629679d61d65daa6762ad67abddc03d061b8
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-07-19 15:03:18 +0200

    Added helper to import and export all images

---
+++ helpers/exportImages.js
@@ -0,0 +1,57 @@
+import RAWDEV_LG from "static/assets/rawdev-lg.gif";
+import RAWDEV_MD from "static/assets/rawdev-md.webp";
+import RAWDEV_SM from "static/assets/rawdev-sm.webp";
+import SDGTC_LG from "static/assets/sdgtc-lg.gif";
+import SDGTC_SM from "static/assets/sdgtc-sm.gif";
+import STATIC_LG from "static/assets/static-website-lg.webp";
+import STATIC_SM from "static/assets/static-website-sm.webp";
+import DATAVIZ_LG from "static/assets/data-visualization-lg.gif";
+import DATAVIZ_SM from "static/assets/data-visualization-sm.webp";
+import DIAPER_LG from "static/assets/diaper-lg.gif";
+import DIAPER_SM from "static/assets/diaper-sm.webp";
+import PLANTHATMEAL_LG from "static/assets/planthatmeal-lg.webp";
+import PLANTHATMEAL_SM from "static/assets/planthatmeal-sm.gif";
+import FINANCIAL_LG from "static/assets/financial-model-lg.gif";
+import FINANCIAL_SM from "static/assets/financial-model-sm.webp";
+import CSS from "static/assets/css3-alt.svg";
+import REACT from "static/assets/react.svg";
+import HTML from "static/assets/html5.svg";
+import JS from "static/assets/js.svg";
+import NEXT from "static/assets/next.svg";
+import NODE from "static/assets/node.svg";
+import EXCEL from "static/assets/excel.svg";
+import MONGO from "static/assets/mongo.svg";
+import TAILWIND from "static/assets/tailwind.svg";
+import GITHUB from "static/assets/github.svg";
+import LINK from "static/assets/link.svg";
+import SHELL from "static/assets/shell.svg";
+
+export const images = {
+  RAWDEV_LG,
+  RAWDEV_MD,
+  RAWDEV_SM,
+  SDGTC_LG,
+  SDGTC_SM,
+  STATIC_LG,
+  STATIC_SM,
+  DATAVIZ_LG,
+  DATAVIZ_SM,
+  DIAPER_LG,
+  DIAPER_SM,
+  PLANTHATMEAL_LG,
+  PLANTHATMEAL_SM,
+  FINANCIAL_LG,
+  FINANCIAL_SM,
+  CSS,
+  REACT,
+  HTML,
+  JS,
+  NEXT,
+  NODE,
+  EXCEL,
+  MONGO,
+  TAILWIND,
+  GITHUB,
+  LINK,
+  SHELL,
+};

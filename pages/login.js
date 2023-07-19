a6eb1b15db979cb22114cb720f9bec88680ace6b
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-07-19 23:42:56 +0200

    updated text color in input for login

--- pages/login.js
+++ pages/login.js
@@ -64,7 +64,7 @@ const Login = () => {
                         type="email"
                         autoComplete="email"
                         required
-                        className="block w-full rounded-sm border-0 py-1.5 shadow-sm bg-transparent ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-tertiary sm:text-sm sm:leading-6"
+                        className="block w-full rounded-sm border-0 py-1.5 shadow-sm bg-transparent ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-tertiary sm:text-sm sm:leading-6 text-gray-50"
                       />
                     </div>
                   </div>
@@ -86,7 +86,7 @@ const Login = () => {
                         type="password"
                         autoComplete="current-password"
                         required
-                        className="block w-full rounded-sm border-0 py-1.5 shadow-sm bg-transparent ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-tertiary sm:text-sm sm:leading-6"
+                        className="block w-full rounded-sm border-0 py-1.5 shadow-sm bg-transparent ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-tertiary sm:text-sm sm:leading-6 text-gray-50"
                       />
                     </div>
                   </div>

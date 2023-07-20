071abad362452f77dd091f1c6b229e9014cc9486
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-07-20 11:08:21 +0200

    test with promise for production

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,10 +1,29 @@
 import { transporter } from "library/nodemailer";
 
-export const sendEmail = (body) => {
-  // send mail with defined transport object
-  transporter.sendMail(setTransportObject(body), (error) => {
-    error ? console.log(error) : console.log("Message sent");
+export const sendEmail = async (body) => {
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
   });
+  // send mail with defined transport object
+  await new Promise((resolve, reject) =>
+    transporter.sendMail(setTransportObject(body), (error, info) => {
+      if (error) {
+        console.log(error);
+        reject(error);
+      }
+      console.log("Message sent");
+      resolve(info);
+    })
+  );
 };
 
 export const setTransportObject = (body) => {

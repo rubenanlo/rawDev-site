f94b7a4c4e049192dc1e4adae24e75144be449f5
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-07-23 22:34:59 +0200

    text-contact form verification

--- pages/api/contact-form/[verifyId].js
+++ pages/api/contact-form/[verifyId].js
@@ -3,13 +3,13 @@ import { connectToDatabase } from "helpers/connectDb";
 
 // Handle the request
 export default async function handler(req, res) {
-  const { formId } = req.query;
+  const { verifyId } = req.query;
   try {
     // Connect to your MongoDB database by accessing the "responses" collection
     const formsCollection = await connectToDatabase("form", "responses");
 
-    // Find the form in the collection based on the formId
-    const form = await formsCollection.findOne({ formId });
+    // Find the form in the collection based on the verifyId
+    const form = await formsCollection.findOne({ verifyId });
 
     // If the form is not found, return an error message
     if (!form) {
@@ -19,12 +19,13 @@ export default async function handler(req, res) {
     // Update the form or perform any other necessary operations
     // Set the "verified" field of the form to true
     await formsCollection.updateOne(
-      { formId },
+      { verifyId },
       { $unset: { verifyId: "" }, $set: { verified: true } }
     );
 
-    // Return a success message
-    res.redirect("/verification");
+    // Perform a server-side redirect
+    res.writeHead(302, { Location: "/verification" });
+    res.end();
   } catch (error) {
     // If an error occurs, log the error and return an error message
     console.error("Error:", error);

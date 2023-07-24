1fa63fd83ddd92efcd92b68d2692a79d1b204065
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-07-24 22:59:48 +0200

    fixed typo in footer

--- components/Footer.js
+++ components/Footer.js
@@ -25,7 +25,7 @@ const navigation = {
     },
     {
       name: "Linkedin",
-      href: "#https://www.linkedin.com/in/ruben-andino/",
+      href: "https://www.linkedin.com/in/ruben-andino/",
       icon: LINKEDIN,
     },
   ],

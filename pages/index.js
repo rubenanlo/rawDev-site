777ad59370d2a4bb893b508462f21e854456b9ff
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-07-27 23:45:17 +0200

    Fixed typo

--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,13 @@ import { images } from "static/homepage/INTRODUCTION";
 import { data } from "static/homepage/TRANSPARENCY";
 import { sections } from "static/homepage/COLLABORATION";
 
-const Homepage = () => {
-  console.log("🚀 ~ file: index.js:8 ~ data:", data);
-
-  return (
-    <AppLayoutWithNavbar>
-      <Introduction images={images} />
-      <Integrity />
-      <Transparency data={data} />
-      <Collaboration sections={sections} />
-    </AppLayoutWithNavbar>
-  );
-};
+const Homepage = () => (
+  <AppLayoutWithNavbar>
+    <Introduction images={images} />
+    <Integrity />
+    <Transparency data={data} />
+    <Collaboration sections={sections} />
+  </AppLayoutWithNavbar>
+);
 
 export default Homepage;

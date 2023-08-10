13e209f5f7b3a4037e1c37a8013d244a20fe4c42
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-08-10 13:13:51 +0200

    fix typo

--- animations/SlideFromBottom.js
+++ animations/SlideFromBottom.js
@@ -1,23 +1,20 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-const SlideFromBottom = ({ children, isInView }) => {
-  console.log(isInView);
-  return (
-    <motion.div
-      initial={{ y: 20, opacity: 0.1 }}
-      animate={{
-        y: isInView ? 0 : 20,
-        opacity: isInView ? 1 : 0,
-        transition: {
-          y: { duration: 1, delay: 0.3 },
-          opacity: { duration: 0.5, delay: 0 },
-        },
-      }}
-    >
-      {children}
-    </motion.div>
-  );
-};
+const SlideFromBottom = ({ children, isInView }) => (
+  <motion.div
+    initial={{ y: 20, opacity: 0.1 }}
+    animate={{
+      y: isInView ? 0 : 20,
+      opacity: isInView ? 1 : 0,
+      transition: {
+        y: { duration: 1, delay: 0.3 },
+        opacity: { duration: 0.5, delay: 0 },
+      },
+    }}
+  >
+    {children}
+  </motion.div>
+);
 
 export default SlideFromBottom;

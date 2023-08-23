cdae5bd8ad513ec69c28f39fe96498f14031fb1a
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2023-08-23 09:19:21 +0200

    fixed typos

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -4,6 +4,7 @@ import ButtonOpenDrawer from "components/dashboard/navigation/ButtonOpenDrawer";
 // import SideBarMobile from "components/dashboard/navigation/SideBarMobile";
 import SideBarDesktop from "components/dashboard/navigation/SideBarDesktop";
 import AdminContent from "components/dashboard/admin-content/AdminContent";
+import { sections, userNavigation } from "static/sidebar/SIDEBAR";
 
 const UserPage = () => {
   return (
@@ -11,12 +12,12 @@ const UserPage = () => {
       {/* <SideBarMobile/> */}
       {/* This div is to make the first row of the table for users with solid background */}
       <div className="absolute w-full h-52 bg-gradient-to-r from-gray-900  to-blue-primary" />
-      <SideBarDesktop />
+      <SideBarDesktop sections={sections} />
       <div className="lg:pl-60 pr-10">
         <div className="fixed z-40 flex h-16 shrink-0 items-center gap-x-4 px-4 sm:gap-x-6 sm:px-6 lg:px-8 right-0">
           <ButtonOpenDrawer />
           <div className="h-6 w-px bg-gray-200 lg:hidden" aria-hidden="true" />
-          <ProfileDropDown />
+          <ProfileDropDown userNavigation={userNavigation} />
         </div>
         <AdminContent />
       </div>
